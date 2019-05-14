25c0087c2ad556809709b34cfa7b3d62f3cc9399
Paji <paji_starone@yahoo.co.id>
2019-05-15 00:55:24 +0700

    query

--- src/index.js
+++ src/index.js
@@ -8,21 +8,52 @@ let dummyPersons = [
 		firstName: 'Michael',
 		lastName: 'Suyama',
 		fullName: 'Michael Suyama',
-		email: 'suyama@wp.co'
+		email: 'suyama@wp.co',
+		likedPosts: [
+			{
+				id: 1,
+				description: 'Introduction to GraphQL',
+				imageUrl: 'google.com'
+			},
+			{
+				id: 2,
+				description: 'Welcome to POC',
+				imageUrl: 'microsoft.com'
+			}
+		]
 	},
 	{
 		id: 2, 
 		firstName: 'Nancy',
 		lastName: 'DaVolio',
 		fullName: 'Nancy DaVolio',
-		email: 'davolio@wp.co'
+		email: 'davolio@wp.co',
+		likedPosts: [
+			{
+				id: 1,
+				description: 'Introduction to GraphQL',
+				imageUrl: 'google.com'
+			}
+		]
 	},
 	{
 		id: 3, 
 		firstName: 'David',
 		lastName: 'Buchanan',
 		fullName: 'David Buchanan',
-		email: 'buchanan@wp.co'
+		email: 'buchanan@wp.co',
+		likedPosts: [
+			{
+				id: 2,
+				description: 'Welcome to POC',
+				imageUrl: 'microsoft.com'
+			},
+			{
+				id: 3,
+				description: 'Advanced GraphQL',
+				imageUrl: 'yahoo.com'
+			}
+		]
 	}
 ]
 
@@ -60,6 +91,7 @@ const typeDefs = `
 		lastName: String
 		fullName: String
 		email: String
+		likedPosts: [Post]
 	}
 	
 	type Post {
@@ -82,7 +114,8 @@ const resolvers = {
 		firstName: (parent) => parent.firstName,
 		lastName: (parent) => parent.lastName,
 		fullName: (parent) => parent.fullName,
-		email: (parent) => parent.email
+		email: (parent) => parent.email,
+		likedPosts: (parent) => parent.likedPosts
 	},
 	Post: {
 		id: (parent) => parent.id,
